89ea23f32dced957d991d8e3ef72ab7f1d821149
tiamabderezai <pass>
2022-01-24 08:21:45 -0500

    initial commit

---
+++ app/src/main/java/com/example/birthdayreminder/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.birthdayreminder
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val c = Calendar.getInstance()
+    val year = c.get(Calendar.YEAR)
+    val month = c.get(Calendar.MONTH)
+    val day = c.get(Calendar.DAY_OF_MONTH)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dpd = DatePickerDialog(this, { view, year, monthOfYear, dayOfMonth ->
+
+            // Display Selected date in textbox
+//            lblDate.setText("" + dayOfMonth + " " + MONTHS[monthOfYear] + ", " + year)
+
+        }, year, month, day)
+
+        dpd.show()
+    }
+}
