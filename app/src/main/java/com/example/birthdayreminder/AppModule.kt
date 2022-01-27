a67e0733eb14eedf73cecf8546759f1691022d61
tiamabderezai <pass>
2022-01-27 15:15:27 -0500

    Added DI

---
+++ app/src/main/java/com/example/birthdayreminder/AppModule.kt
@@ -0,0 +1,7 @@
+package com.example.birthdayreminder
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppModule : Application() {
+}
--- app/src/main/java/com/example/birthdayreminder/view/MainActivity.kt
+++ app/src/main/java/com/example/birthdayreminder/view/MainActivity.kt
@@ -6,24 +6,7 @@ import android.os.Bundle
 import com.example.birthdayreminder.R
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+import dagger.hilt.android.AndroidEntryPoint
 
-    val c = Calendar.getInstance()
-    val year = c.get(Calendar.YEAR)
-    val month = c.get(Calendar.MONTH)
-    val day = c.get(Calendar.DAY_OF_MONTH)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val dpd = DatePickerDialog(this, { view, year, monthOfYear, dayOfMonth ->
-
-            // Display Selected date in textbox
-//            lblDate.setText("" + dayOfMonth + " " + MONTHS[monthOfYear] + ", " + year)
-
-        }, year, month, day)
-
-        dpd.show()
-    }
-}
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
